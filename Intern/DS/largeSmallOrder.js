4fc63ac227d16565356fb677b3867fcec6cea592
vignesh222002 <113511382+vignesh222002@users.noreply.github.com>
2023-02-01 22:14:11 +0530

    Delete Intern/DS directory

--- Intern/DS/largeSmallOrder.js
+++
@@ -1,9 +0,0 @@
-let array = [9,7,8,6,5,4,1,2,3,2,1,7,5];
-array.sort(function(a, b){return a - b});
-let arr = [...new Set (array)];
-
-for (let i = 1; i < arr.length - 1; i += 2) {
-    [arr[i], arr[i+1]] = [arr[i+1], arr[i]];
-}
-
-console.log(arr);
